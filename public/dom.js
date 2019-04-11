f62972c3ad9e2d2cbb329accea99b30359988b83
Samantha Dickeson <samlucydickeson@gmail.com>
2019-04-11 19:40:36 +0100

    add button check

--- public/dom.js
+++ public/dom.js
@@ -9,9 +9,25 @@ const updateDom = array => {
   });
 };
 
+const updateButton = num => {
+  console.log(typeof num);
+  const button = document.getElementById("submit-button");
+  if (num === 0) {
+    button.disabled = true;
+    button.textContent = "sorry, we're full!";
+  }
+};
+
 const getRooms = () => {
   fetch("/rooms")
     .then(res => res.json())
     .then(response => updateDom(response));
 };
+
+const getFrees = () => {
+  fetch("/frees")
+    .then(res => res.json())
+    .then(response => updateButton(response));
+};
 getRooms();
+getFrees();
