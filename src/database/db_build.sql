cb80091bd5d98e04d35bdf45bc7396ff43a274c4
Samantha Dickeson <samlucydickeson@gmail.com>
2019-04-11 14:29:27 +0100

    add rooms table to database

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -14,11 +14,37 @@ CREATE TABLE guinea_pigs
   room_service TEXT
 );
 
+CREATE TABLE rooms
+(
+  room_id SERIAL PRIMARY KEY,
+  room_num INTEGER NOT NULL,
+  guinea_id INTEGER,
+  occupied BIT NOT NULL,
+  CONSTRAINT guinea_id FOREIGN KEY (guinea_id) REFERENCES guinea_pigs (guinea_id)
+);
+
 INSERT INTO guinea_pigs
   (guinea_name, guinea_colour, gender, here, room_num)
 VALUES
   ('Fluffles', 'Ginger', 'F', '1', 101),
   ('Sparks', 'Brown', 'M', '1', 102),
   ('Spot', 'Black and white', 'M', '1', 205);
+
+
+INSERT INTO rooms
+  (
+  room_num, occupied
+  )
+VALUES
+  (101, '0'),
+  (102, '0'),
+  (103, '0'),
+  (104, '0'),
+  (105, '0'),
+  (201, '0'),
+  (202, '0'),
+  (203, '0'),
+  (204, '0'),
+  (205, '0');
 
 COMMIT;
